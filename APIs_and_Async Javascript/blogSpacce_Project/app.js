c5572d503aeca58bac6a9a930d19a01768bda1fc
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-07-25 12:08:50 +0500

    Update the file

--- APIs_and_Async Javascript/blogSpacce_Project/app.js
+++ APIs_and_Async Javascript/blogSpacce_Project/app.js
@@ -1,19 +1,45 @@
-// to get the first five posts 
+let postsArray = []
 
-const FivePosts = document.getElementById("Five-Posts")
-FivePosts.innerHTML = 
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-    const postsArr = data.slice(0, 5)
-    let html = ''
-    postsArr.forEach(post => 
-    {
-        html += ` 
-        <h2>${post.title}</h2>
-        <p>${post.body}</p>
-        `;
-    });
-    document.getElementById("Five-Posts").innerHTML = html;
-        
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+        })
+})
