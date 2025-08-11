0a616680b0e15e7984fbfb1f0f74b99a172692a8
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-08-11 18:53:41 +0500

    now fetching actual gold price
    -> replace hardcoded gold price with live data fetched via API using node fetch.

--- Node_Js/gold_digger_soloProject/handlers/apiHandler.js
+++ Node_Js/gold_digger_soloProject/handlers/apiHandler.js
@@ -1,14 +1,17 @@
 // GOld price constant we will make it dynamic later
-
+import fetch  from "node-fetch";
 const CURRENT_GOLD_PRICE = 2345.89;
 
 // Handle GET /api/gold-price
 
-export function handleGoldPriceAPI(req , res) {
+export async function handleGoldPriceAPI(req , res) {
+    const url = new URL(req.url , `http://${req.headers.host}`);
+    const currency = url.searchParams.get('currency') || 'GBP';
     const responseData =
     {
-        price : CURRENT_GOLD_PRICE,
-        currency : 'GBP' ,
+        
+        price : await fetchRealGoldPrice(currency),
+        currency : 'currency' ,
         unit : 'per troy ounce' , 
         timestamp: new Date().toISOString()
     };
@@ -20,19 +23,19 @@ export function handleGoldPriceAPI(req , res) {
 
 // Handle POST /api/invest 
 
-export function handleInvestmentAPI(req , res) {
+export async function handleInvestmentAPI(req , res) {
     let body = '';
 
     req.on('data' , (chunk) => {
         body += chunk.toString();
     });
 
-    req.on('end' , () => {
+    req.on('end' ,async () => {
         try 
         {
             const investmentData = JSON.parse(body);
             const amount = parseFloat(investmentData.amount);
-            const goldPrice = CURRENT_GOLD_PRICE;
+            const goldPrice = await fetchRealGoldPrice('GBP');
 
             const ounces = amount / goldPrice;
 
@@ -56,4 +59,31 @@ export function handleInvestmentAPI(req , res) {
 
         }
     })
+}
+
+async function fetchRealGoldPrice(currency = 'GBP'){
+    try
+    {
+        const response = await fetch(`https://api.metalpriceapi.com/v1/latest?api_key=a9c14181fe151d81681c0380d2435b0a&base=${currency}&currencies=XAU`);
+        const data = await response.json();
+
+        console.log('API Response:' , data)
+        const xauPerCurrency = data.rates.XAU; 
+        if (!xauPerCurrency || xauPerCurrency <= 0) {
+        throw new Error('Invalid XAU rate');
+    }
+        const priceInCurrency = 1 / xauPerCurrency;
+        return parseFloat(priceInCurrency.toFixed(2));
+    }
+    catch(error)
+    {
+        console.log("error in fetching real price of gold ", error.message);
+        const fallbackPrices = {
+            'GBP': 2345.89,
+            'USD': 2899.50,
+            'EUR': 2677.25
+        };
+        return fallbackPrices[currency] || fallbackPrices['GBP'];
+
+    }
 }
