6670698beb7bc2a99acca96dc37cf08230bf745e
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-08-11 18:44:50 +0500

    update the file
    before the server file has much code so we call functions here ..

--- Node_Js/gold_digger_soloProject/server.js
+++ Node_Js/gold_digger_soloProject/server.js
@@ -2,25 +2,12 @@ import http, { createServer } from 'node:http';
 import fs from 'node:fs';    // for reading files 
 import path from 'node:path'; // for handling file paths
 import url from 'node:url';  // for parsing urls
+import { handleGoldPriceAPI , handleInvestmentAPI } from './handlers/apiHandler.js';
+import {handleStaticFiles} from './handlers/staticHandler.js';
 
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url)); // gives us current directory path name
-function serveStaticFile(res , filePath , contentType){
-    fs.readFile(filePath , (err , data) => {
-    if(err)
-    {
-        res.statusCode = 500;
-        res.end('server error');
-        return;
-    }
-    else
-    {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', contentType)
-        res.end(data);
-    }
-})
-}
+
 
 const PORT = 3000;
 const server = http.createServer((req , res) => 
@@ -28,32 +15,13 @@ const server = http.createServer((req , res) =>
     const parsedUrl = url.parse(req.url , true);
     const pathName = parsedUrl.pathname;
     console.log('Requested' , pathName);
-    if (pathName == '/'  || pathName == '/index.html')
-    {
-        const filePath = path.join(__dirname , 'public' , 'index.html')
-        serveStaticFile(res , filePath , 'text/html')
-    }
-    else if(pathName == '/index.css')
-            {
-                const filePath = path.join(__dirname , 'public' , 'index.css')
-                serveStaticFile(res , filePath , 'text/CSS')
-            }
-    else if(pathName == '/gold.png')
-            {
-                const filePath = path.join(__dirname , 'public' , 'gold.png')
-                serveStaticFile(res , filePath , 'image/png')
-            }
-    else if(pathName == '/index.js')
-    {
-        const filePath = path.join(__dirname , 'public' , 'index.js')
-        serveStaticFile(res , filePath , 'application/javascript')
-    }
-        
-    else if(pathName == '/test-api.html') {
-    const filePath = path.join(__dirname, 'public', 'test-api.html');
-    serveStaticFile(res, filePath, 'text/html');
-    }
+    const method = req.method;
+    console.log('Method:' , method , 'path:' , pathName);
 
+    if (handleStaticFiles(req, res, pathName, __dirname)) 
+    {
+    
+    }
     else if(pathName == '/api/gold-price')
     {
        handleGoldPriceAPI(req , res);
