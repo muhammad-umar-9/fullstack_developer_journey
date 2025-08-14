c110a2c2b6e289922b9bebf0f025f1fc2ad517ae
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-08-14 22:53:48 +0500

    interest component

---
+++ React_Js_Fundamentals/static_Pages/Buisness_Card_Solo_Project/src/componenets/interests.jsx
@@ -0,0 +1,13 @@
+export default function Interests()
+{
+    return(
+        <div>
+        <h3 className="section-heading">Interests</h3>
+        <p>
+            Data wizard. Machine learning enthusiast. 
+            Code crafter. Insight seeker. Tech explorer. Lifelong learner.
+            Problem solver. Innovation chaser.
+        </p>
+        </div>
+    )
+}
