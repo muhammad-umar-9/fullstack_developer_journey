bfbf7ea630000be772eca67ae1eac7b7ffa99806
Muhammad Umar <167021629+muhammad-umar-9@users.noreply.github.com>
2025-08-14 22:50:23 +0500

    about component for buisness card

---
+++ React_Js_Fundamentals/static_Pages/Buisness_Card_Solo_Project/src/componenets/about.jsx
@@ -0,0 +1,13 @@
+export default function About()
+{
+    return(
+        <div>
+        <h3 className="section-heading">About</h3>
+        <p>
+            I am a frontend developer passionate about creating simple, user-friendly solutions and automating everyday tasks. 
+            I stay updated on security and best practices, and I am constantly seeking 
+            opportunities to expand my skills and explore new technologies.
+        </p>
+        </div>
+    )
+}
